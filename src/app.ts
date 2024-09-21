91c2178d12fe7dbfeaec1ac572d5187e319f221d
Vladimir Sitchikhin <volodya95rus@icloud.com>
2024-09-21 15:42:40 +0300

    fix: Правка CORS

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,15 @@ import dotenv from 'dotenv';
 import {db} from './database/db';
 import getNotesRouter from "./routes/notes.ts";
 import getTodosRouter from "./routes/todos.ts";
+import cors from 'cors';
 
 dotenv.config();
 
 export const app: Express = express();
 
 app.use(express.json());
+app.use(cors({
+  origin: '*'
+}));
 app.use('/notes', getNotesRouter(db));
 app.use('/todos', getTodosRouter(db));
